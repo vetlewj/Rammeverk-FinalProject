39ec8001b5ddeaa859f058ff439a6fa5a4aa20f3
vetlewj <vetlewj@gmail.com>
2022-04-04 20:27:31 +0200

    Create ScraperNotInitializedException.java

---
+++ src/main/java/scraper/exceptions/ScraperNotInitializedException.java
@@ -0,0 +1,11 @@
+package scraper.exceptions;
+
+public class ScraperNotInitializedException extends Exception{
+    public ScraperNotInitializedException() {
+        super("Scraper has not been initialized");
+    }
+
+    public ScraperNotInitializedException(String message) {
+        super(message);
+    }
+}
