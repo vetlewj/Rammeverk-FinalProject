7c56ed064f641f9856b085c1254d722c5053867b
vetlewj <vetlewj@gmail.com>
2022-04-04 17:39:58 +0200

    Getters and setters for HtmlElement

--- src/main/java/scraper/html/HtmlElement.java
+++ src/main/java/scraper/html/HtmlElement.java
@@ -1,16 +1,21 @@
 package scraper.html;
 
+import org.jsoup.nodes.Attributes;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /** Represents a HTML Tag.
  * @author Vetle Jahr
  * @version 1.0
  */
 public class HtmlElement {
-    private HtmlElement rootElement;
     private String tag;
     private HashMap<String, String> attributes;
+
     /**
      * Represents the child HTML elements. Only contains the direct children.
      */
@@ -27,6 +32,38 @@ public class HtmlElement {
 
     public HtmlElement getChildByXPath(String s) {
         return null;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public HashMap<String, String> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(HashMap<String, String> attributes) {
+        this.attributes = attributes;
+    }
+
+    public ArrayList<HtmlElement> getChildElements() {
+        return childElements;
+    }
+
+    public void setChildElements(ArrayList<HtmlElement> childElements) {
+        this.childElements = childElements;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 
     @Override
