f8fa924384c1c2c1d9510b7d30ba80da033de445
vetlewj <vetlewj@gmail.com>
2022-05-04 18:34:48 +0200

    Test for creating Scraper object from file

---
+++ src/test/java/integrationtests/ScraperFileTest.java
@@ -0,0 +1,21 @@
+package integrationtests;
+
+import org.junit.jupiter.api.Test;
+import scraper.Scraper;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public class ScraperFileTest {
+
+    @Test
+    void buildScraperWithHtmlFile(){
+        File testFile = new File("src/test/resources/snlMichelangelo.html");
+        Scraper scraper = Scraper.buildScraperWithHtmlFile(testFile);
+        assertNotNull(scraper);
+        assertSame(scraper.getClass(), Scraper.class);
+    }
+
+}
