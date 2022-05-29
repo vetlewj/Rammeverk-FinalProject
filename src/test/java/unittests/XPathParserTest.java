e1903c892ce0c853270f40bb02eeef106f4bf340
vetlewj <vetlewj@gmail.com>
2022-05-29 22:50:46 +0200

    remove unused comments

--- src/test/java/unittests/XPathParserTest.java
+++ src/test/java/unittests/XPathParserTest.java
@@ -14,38 +14,3 @@ public class XPathParserTest {
     }
 
 }
-// Alternate implementation of XpathToCssSelector method
-//                    // if tagname
-//                    else if (Pattern.matches("[a-zA-Z]", xPathSplit[i+2])) {
-//                        // TODO: implement
-//                    }
-//                    // if parent
-//                    else if(xPathSplit[i+2].equals(parent)) {
-//                        // TODO: implement
-//                    }
-//                    // if an attribute
-//                    else if (xPathSplit[i + 2].equals(attribute)) {
-//                        String key = xPathSplit[i + 3];
-//                        if (xPathSplit[i + 4].equals(attributeEquals)) {
-//                            String value = xPathSplit[i + 5];
-//                            // elements = elements.select(parent + key + attribute + value);
-//                            i = i + 5;
-//                        } else {
-//                            throw new InvalidXPathException("Invalid xpath: " + xPath + "Attribute value not found " +
-//                                                            "for key: " + key);
-//                        }
-//                    }
-//
-//
-//                    else if(xPathSplit[i+1].equals(attribute)) {
-//                    String key = xPathSplit[i+2];
-//                    if(xPathSplit[i+3].equals(attributeEquals)) {
-//                        String value = xPathSplit[i+4];
-//                        // elements = elements.select(parent + key + attribute + value);
-//                        i = i + 4;
-//                    } else {
-//                        throw new InvalidXPathException("Invalid xpath: " + xPath + "Attribute value not found " +
-//                                                        "for key: " + key);
-//                    }
-//                }
-
