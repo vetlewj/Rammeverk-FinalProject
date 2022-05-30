5d5fcd3b2310bea30b78c91a89fd21953eba83be
vetlewj <vetlewj@gmail.com>
2022-05-30 12:27:42 +0200

    JavaDoc for ProductTemplate

--- src/main/java/scraper/templatescraper/ProductTemplate.java
+++ src/main/java/scraper/templatescraper/ProductTemplate.java
@@ -1,25 +1,59 @@
 package scraper.templatescraper;
 
-import scraper.templatescraper.TemplateScraper;
-
 /**
  * @author Vetle Jahr
  * @version 1.0
  */
 public abstract class ProductTemplate extends TemplateScraper {
 
+    /**
+     * Gets the name of the product.
+     *
+     * @return name of the product
+     */
     public abstract String getName();
 
+    /**
+     * Gets the price of the product.
+     *
+     * @return price of the product
+     */
     public abstract float getPrice();
 
+    /**
+     * Gets the description of the product.
+     *
+     * @return description of the product
+     */
     public abstract String getDescription();
 
+    /**
+     * Gets the url to the image of the product.
+     *
+     * @return url to image of the product
+     */
     public abstract String getImage();
 
+    /**
+     * Gets the manufacturer of the product
+     *
+     * @return manufacturer of the product
+     */
     public abstract String getManufacturer();
 
+    /**
+     * Gets the category that the product belongs to.
+     *
+     * @return category that the product belongs to
+     */
     public abstract String getCategory();
 
+    /**
+     * Creates a ProductTemplate object from a template. To be created with an anonymous class that is used as parameter
+     *
+     * @param template template to create ProductTemplate from.
+     * @return ProductTemplate object created from template.
+     */
     public static ProductTemplate buildTemplate(ProductTemplate template) {
         return template;
     }
