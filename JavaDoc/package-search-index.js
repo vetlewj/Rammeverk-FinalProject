271a4bc759c26b532a99a2c41db3fdf6ab1b9951
vetlewj <vetlewj@gmail.com>
2022-05-31 21:52:12 +0200

    Generated JavaDoc

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"scraper"},{"l":"scraper.exceptions"},{"l":"scraper.html"},{"l":"scraper.templatescraper"},{"l":"scraper.templatescraper.templates"},{"l":"scraper.xpathparser"}];updateSearchResults();
